336e9f44248bc643a48b14bcdf912b4a053249bd
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-08-10 23:02:02 -0400

    first

---
+++ quiz-game-java-api/src/main/java/com/quizgame/quizgamejavaapi/QuizGameJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.quizgame.quizgamejavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGameJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGameJavaApiApplication.class, args);
+	}
+
+}
---
+++ quiz-game-java-api/src/test/java/com/quizgame/quizgamejavaapi/QuizGameJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizgame.quizgamejavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGameJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
