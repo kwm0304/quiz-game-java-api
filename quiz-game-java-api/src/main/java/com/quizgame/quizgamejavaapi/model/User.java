47b3e3595d88c251f5e72230a7348f9a057401ce
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-08-10 23:10:49 -0400

    startged user model

---
+++ quiz-game-java-api/src/main/java/com/quizgame/quizgamejavaapi/model/User.java
@@ -0,0 +1,18 @@
+package com.quizgame.quizgamejavaapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String username;
+    private String password;
+    @Transient
+    boolean loggedIn;
+    private List<Scores> scores;
+}
